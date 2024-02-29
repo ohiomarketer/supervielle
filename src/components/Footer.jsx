b2ccb601911edc52563e46f54db8980936bc4af9
ohiomarketer <phonesmarketer@gmail.com>
2024-02-29 15:21:09 -0300

    asd

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { styled } from 'styled-components'
-import footerimg from '../assets/static/footer.png';
+import footerimg from '../assets/static/s.png';
 
 export const Footer = () => {
   return (
@@ -11,11 +11,13 @@ export const Footer = () => {
 }
 
 const FooterContainer = styled.div`
-    width: 100%;
-    height: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    z-index: 100;
 
         img {
             width: 100%;
