e9a2276e1ebee34ecae19c1edc4e6280a4fc4d3c
ohiomarketer <phonesmarketer@gmail.com>
2024-02-29 17:23:05 -0300

    asddas

--- src/components/Movements.jsx
+++ src/components/Movements.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import movementsimage from '../assets/static/movimientos.png'
+import loader from '../assets/static/movimientos2.png'
 import { styled } from 'styled-components';
 
 export const Movements = () => {
+    const [loading, setLoading] = useState(false);
+
+    const loadAndBack = () => {
+        setLoading(true);
+        setTimeout(() => {
+            setLoading(false);
+        }, 1000)
+    }
+
+if (loading) {
+    return (
+        <MovementsContainer onClick={() => loadAndBack()}>
+            <img src={loader} alt="" />
+        </MovementsContainer>
+        )
+    }
   return (
-    <MovementsContainer>
+    <MovementsContainer onClick={() => loadAndBack()}>
         <img src={movementsimage} alt="" />
     </MovementsContainer>
   )
