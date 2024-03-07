d36428009fd3347562a6805242dc638ec947fe84
ohiomarketer <phonesmarketer@gmail.com>
2024-03-07 10:52:17 -0300

    as

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import servicesimage from '../assets/static/servicios3.png'
+import servicesimage from '../assets/static/servicios4.png'
 import { styled } from 'styled-components';
 
 export const Services = () => {
